7d4b9ebc72ff00d4802bbb629b629c0f5951cc67
rawatap2003 <149507725+rawatap2003@users.noreply.github.com>
2025-03-31 00:42:34 +0530

    Initial commit

---
+++ public/routes/surveyRoutes.js
@@ -0,0 +1,30 @@
+const mongoose=require('mongoose');
+const requireLogin=require('../middlewares/requireLogin');
+const Mailer=require('../services/Mailer');
+const surveyTemplate=require('../services/emailTemplates/surveyTemplate');
+const Survey=mongoose.model('surveys');
+module.exports=app=>{
+    app.get('/api/surveys',(req,res)=>{
+        res.send('thanks for voting!');
+    });
+    app.post('/api/surveys',requireLogin,async (req,res)=>{
+        const{title,subject,body,recipients}=req.body;
+        const survey=new Survey({
+            title,
+            subject,
+            body,
+            recipients: recipients.split(',').map(email => ({ email: email.trim() })),
+            _user:req.user.id,
+            dateSent:Date.now()
+        });
+        const mailer=new Mailer(survey,surveyTemplate(survey));
+        try{
+            await mailer.send();
+            await survey.save();
+            const user=await req.user.save();
+            res.send(user);
+        }catch(err){
+            res.status(422).send(err);
+        }
+    });
+};
